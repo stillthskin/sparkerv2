1d6683e19ae04581843073a6f42936d6bd781ac1
Dennis <kenda.dennis3@gmail.com>
2025-06-29 11:30:33 +0300

    Urls error

--- sparkermain/urls.py
+++ sparkermain/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     
     path('', views.login_view, name='login') , 
-    path('/login', views.login_view, name='login') , 
-    path('/home/', views.home, name='home') ,
+    path('login', views.login_view, name='login') , 
+    path('home/', views.home, name='home') ,
     # path('train_model/', views.train_model, name='train_model'),
 ]
